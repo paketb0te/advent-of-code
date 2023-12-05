8da9b54d03c0bc8f7c81e2771d13c001ef0ccf1b
Manuel Stausberg <stausberg@denic.de>
2023-12-05 22:27:25 +0100

    day 5 part 1

--- 2023/05/solve.py
+++ 2023/05/solve.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+import itertools
 from pathlib import Path
 
 INPUT = "input.txt"
@@ -10,22 +11,33 @@ def get_lines(filename: str) -> list[str]:
         return fh.read().splitlines()
 
 
-@dataclass
+@dataclass(frozen=True)
 class MapEntry:
     dst_start: int
     src_start: int
-    range_length: int
+    range: int
 
-    def map(self, src: int) -> int:
-        if not self._value_in_range(src):
-            raise ValueError(
-                f"src out of bounds: {self.src_start=} {self.range_length=}"
-            )
+    def map(self, value: int) -> int:
+        if not self.value_in_range(value):
+            raise ValueError(f"src out of bounds: {self.src_start=} {self.range=}")
         offset = self.dst_start - self.src_start
-        return src + offset
+        return value + offset
 
-    def _value_in_range(self, value: int) -> bool:
-        return self.src_start <= value <= self.src_start + self.range_length - 1
+    def value_in_range(self, value: int) -> bool:
+        return self.src_start <= value <= self.src_start + self.range - 1
+
+
+@dataclass()
+class Map:
+    source: str
+    target: str
+    entries: list[MapEntry] = field(default_factory=list)
+
+    def map(self, value: int) -> int:
+        for entry in self.entries:
+            if entry.value_in_range(value):
+                return entry.map(value)
+        return value
 
 
 def parse_mapping_name(line: str) -> tuple[str, str]:
@@ -39,16 +51,79 @@ def parse_seeds(line: str) -> list[int]:
     return [int(n) for n in seeds.split()]
 
 
-def parse_map_line(line: str) -> MapEntry:
+def parse_map_entry_line(line: str) -> MapEntry:
     dst_start, src_start, range_length = line.split()
     return MapEntry(
         dst_start=int(dst_start),
         src_start=int(src_start),
-        range_length=int(range_length),
+        range=int(range_length),
     )
 
 
+def build_maps_from_lines(lines: list[str]) -> list[Map]:
+    maps: list[Map] = []
+
+    current_map: Map | None = None
+    current_entry: MapEntry | None = None
+    current_entries: list[MapEntry] = []
+
+    for line in lines:
+        if line.endswith("map:"):
+            source, target = parse_mapping_name(line)
+            current_map = Map(source=source, target=target)
+        elif line == "" and current_map is not None:
+            current_map.entries = current_entries
+            maps.append(current_map)
+            current_entries = []
+            current_map = None
+        else:
+            current_entry = parse_map_entry_line(line)
+            current_entries.append(current_entry)
+
+    return maps
+
+
+def calculate(value: int, maps: list[Map]) -> int:
+    for m in maps:
+        value = m.map(value)
+    return value
+
+
+def part_1(filename: str):
+    lines = get_lines(filename)
+    seeds = parse_seeds(line=lines[0])
+
+    # Append an empty line so the loop in
+    # build_maps_from_lines() recognizes the last Map
+    lines.append("")
+
+    maps = build_maps_from_lines(lines=lines[2:])
+
+    locations: list[int] = []
+    for seed in seeds:
+        location = calculate(value=seed, maps=maps)
+        locations.append(location)
+
+    return min(locations)
+
+
+### Part 2
+
+
+@dataclass
+class Range:
+    start: int
+    length: int
+
+
+def parse_seeds_as_ranges(line: str) -> list[Range]:
+    values = parse_seeds(line)
+    return [
+        Range(start=start, length=length)
+        for start, length in itertools.batched(values, 2)
+    ]
+
+
 if __name__ == "__main__":
-    ...
-    # print("Part 1:", part_1(INPUT))
+    print("Part 1:", part_1(INPUT))
     # print("Part 2:", part_2(INPUT))
--- 2023/05/test_solve.py
+++ 2023/05/test_solve.py
@@ -20,21 +20,116 @@ def test_parse_seeds():
 
 def test_parse_map_line():
     given = "0 15 37"
-    want = solve.MapEntry(dst_start=0, src_start=15, range_length=37)
-    got = solve.parse_map_line(given)
+    want = solve.MapEntry(dst_start=0, src_start=15, range=37)
+    got = solve.parse_map_entry_line(given)
     assert got == want
 
 
-def test_entry_map():
-    entry = solve.MapEntry(dst_start=0, src_start=15, range_length=10)
-
-    given = [15, 16, 24]
-    want = [0, 1, 9]
+def test_entry_map_returns_correct_value_in_range():
+    entry = solve.MapEntry(dst_start=10, src_start=15, range=3)
+    given = [15, 16, 17]
+    want = [10, 11, 12]
 
     for g, w in zip(given, want):
         assert entry.map(g) == w
 
+
+def test_entry_map_raises_on_value_out_of_range():
+    entry = solve.MapEntry(dst_start=10, src_start=15, range=3)
     with pytest.raises(ValueError):
         entry.map(14)
     with pytest.raises(ValueError):
-        entry.map(25)
+        entry.map(18)
+
+
+def test_select_correct_map_entry():
+    map_entries = [
+        solve.MapEntry(dst_start=100, src_start=5, range=5),
+        solve.MapEntry(dst_start=200, src_start=10, range=5),
+        solve.MapEntry(dst_start=300, src_start=15, range=5),
+    ]
+    given = 12
+    want = [False, True, False]
+    got = [m.value_in_range(given) for m in map_entries]
+    assert got == want
+
+
+def test_map_input():
+    map_entries = [
+        solve.MapEntry(dst_start=100, src_start=10, range=5),
+        solve.MapEntry(dst_start=200, src_start=20, range=5),
+    ]
+    m = solve.Map("a", "b", map_entries)
+
+    given = 22
+    want = 202
+    got = m.map(given)
+    assert got == want
+
+
+def test_map_input_out_of_range():
+    map_entries = [
+        solve.MapEntry(dst_start=100, src_start=10, range=5),
+        solve.MapEntry(dst_start=200, src_start=20, range=5),
+    ]
+    m = solve.Map("a", "b", entries=map_entries)
+
+    given = [1, 18, 999]
+    want = [1, 18, 999]
+    for g, w in zip(given, want):
+        assert m.map(g) == w
+
+
+def test_build_seed_to_soil_map():
+    given = [
+        "seed-to-soil map:",
+        "50 98 2",
+        "52 50 48",
+        "",
+    ]
+    map_entries = [
+        solve.MapEntry(dst_start=50, src_start=98, range=2),
+        solve.MapEntry(dst_start=52, src_start=50, range=48),
+    ]
+
+    want = solve.Map(source="seed", target="soil", entries=map_entries)
+    got = solve.build_maps_from_lines(given)[0]
+
+    assert got == want
+
+
+def test_chaining_maps():
+    map_1_entries = [
+        solve.MapEntry(dst_start=50, src_start=98, range=2),
+        solve.MapEntry(dst_start=52, src_start=50, range=48),
+    ]
+    map_1 = solve.Map(source="seed", target="soil", entries=map_1_entries)
+    map_2_entries = [
+        solve.MapEntry(dst_start=0, src_start=15, range=37),
+        solve.MapEntry(dst_start=37, src_start=52, range=2),
+        solve.MapEntry(dst_start=39, src_start=0, range=15),
+    ]
+    map_2 = solve.Map(source="seed", target="soil", entries=map_2_entries)
+    maps = [map_1, map_2]
+
+    given = 50
+    want = 37
+    got = solve.calculate(given, maps)
+
+    assert got == want
+
+
+def test_part_1():
+    want = 35
+    got = solve.part_1(TEST_INPUT)
+    assert got == want
+
+
+def test_parse_seeds_as_ranges():
+    given = "seeds: 79 14 55 13"
+    want = [
+        solve.Range(start=79, length=14),
+        solve.Range(start=55, length=13),
+    ]
+    got = solve.parse_seeds_as_ranges(given)
+    assert got == want
